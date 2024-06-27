c9fac1a789e412df2e9c19d13d4b3f3089819d7e
es1210 <dmstn27034@gmail.com>
2024-06-27 14:51:40 +0900

    문제2

--- src/main/java/org/koreait/Main.java
+++ src/main/java/org/koreait/Main.java
@@ -5,17 +5,21 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
 
-        int a = sc.nextInt();
-        int b = sc.nextInt();
+        for (int i = n; i >= 1; i--) {
 
-        for (int i = 0; i < b; i++) {
-            for (int j = 0; j < a; j++) {
-                System.out.print("*");
+            for (int j = 1; j <= n - i; j++) {
+                System.out.print(" ");
             }
-            System.out.println("");
+
+            for (int j = i; j >= 1; j--) {
+                System.out.print(j + " ");
+            }
+            System.out.println();
         }
 
         sc.close();
+
     }
 }
